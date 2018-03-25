fa7e111c290bde9bc0e81020731457e9506790ad
Miu Golladay <mgolladay@snap-software.com>
2018-03-24 21:06:54 -0700

    Production topic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
@@ -8,8 +7,7 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+    btn.innerHTML = 'Button';
 
     element.appendChild(btn);
  
