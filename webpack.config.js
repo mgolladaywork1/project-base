751aaf49779d6dbfc6a19cc1753950f83a694d8d
Miu Golladay <mgolladay@snap-software.com>
2018-03-24 20:04:35 -0700

    Tree Shaking, set mode production

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  }, 
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new ManifestPlugin()
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  mode: "production"
 };
