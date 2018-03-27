26bc6c7d1997d5785021283d20ca5c6d6aeea229
Miu Golladay <mgolladay@snap-software.com>
2018-03-27 16:38:20 -0700

    start TypeScript Topics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
@@ -9,8 +8,7 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
+    
     element.appendChild(btn);
  
     return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,21 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  }, 
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new ManifestPlugin()
-  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
