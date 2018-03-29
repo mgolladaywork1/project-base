731b481f735f6dce45b06e457b12a508fe83ffdb
Miu Golladay <mgolladay@snap-software.com>
2018-03-28 22:12:18 -0700

    css and LESS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
+import './style.less';
 
 function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
+    var element2 = document.createElement('p');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
     element.appendChild(btn);
+
+    element2.innerText = 'Hello LESS!';
+    element2.id = 'header';
+
+    element.appendChild(element2);
  
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css');
+const extractLESS = new ExtractTextPlugin('stylesheets/[name]-two.css');
 module.exports = {
   entry: {
     app: './src/index.js',
@@ -12,11 +15,25 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: extractCSS.extract([ 'css-loader' ])
+      },
+      {
+        test: /\.less$/i,
+        use: extractLESS.extract([ 'css-loader', 'less-loader'])
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     }),
+    extractCSS,
+    extractLESS,
     new ManifestPlugin()
   ],
   output: {
