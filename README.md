7f8a9c772d1ee734372573a5a23d9fc383c22a58
Jeff Weyer <jweyer@snap-software.com>
2018-04-06 20:45:53 -0700

    Deleted all of the files to prepare for rebuild.

--- README.md
+++
@@ -1,4 +0,0 @@
-Start Webpack 4.1.1
-Build from scratch
-work on git branch
-Try using SSH
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-function component() {
-    var element = document.createElement('div');
-    element.innerHTML = 'Hello';
-    return element;
-  }
-  
-  document.body.appendChild(component());
