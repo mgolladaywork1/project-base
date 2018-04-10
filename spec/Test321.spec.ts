e5787f77e0c4a109a003634a38871b7a6b7ce711
Miu Golladay <mgolladay@snap-software.com>
2018-04-09 21:52:21 -0700

    added more tests method

--- spec/Test321.spec.ts
+++ spec/Test321.spec.ts
@@ -1,13 +1,29 @@
 import { Test321 } from "../src/Test321";
 
 describe("Test321", () => {
+  let test2 = new Test321("Is it True?");
+ 
   it("Test001 to be True", () => {
     let test1 = new Test321("It is True");
 
     let result = test1.test001();
 
     expect(result).toBe(true);
-  })
+  });
 
+  it("Test002 to be not True", () => {
+    let result = test2.test002();
+    expect(result).not.toBe(true);
+  });
+
+  describe("To Be using ===", () => {
+    it("to be the same, true", () => {
+      let t1 = test2.test003();
+      let t2 = t1;
+
+      expect(t2).toBe(t1);
+      expect(t1).not.toBe(null);
+    });
+  });
 
 });
--- src/Test321.ts
+++ src/Test321.ts
@@ -5,4 +5,14 @@ export class Test321 {
   test001(): boolean {
       return true;
   }
+
+  test002(): boolean {
+    let a: number = 1;
+    let b: number = 1 + 1;
+    return ( a == b );
+  }
+
+  test003(): number {
+    return 1;  
+  }
 }
